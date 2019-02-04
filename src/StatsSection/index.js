ae7ff0d073624c9e775aa4b9beaa04e542dcec6f
jshaw <jshaw64@gmail.com>
2019-02-03 21:39:47 -0500

    Replace example Card with placeholder stats in StatsSection

--- src/StatsSection/index.js
+++ src/StatsSection/index.js
@@ -36,27 +36,13 @@ function SimpleCard(props) {
           color="textSecondary"
           gutterBottom
         >
-          Word of the Day
+          In the last 6 months
         </Typography>
         <Typography variant="h5" component="h2">
-          be
-          {bull}
-          nev
-          {bull}o{bull}
-          lent
+          I spent...
         </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <Typography component="p">$100 on Dunkin'</Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 }
