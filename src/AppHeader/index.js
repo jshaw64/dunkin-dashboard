8810ff5db3755487e2880aa1cc810e3262dd0582
jshaw <jshaw64@gmail.com>
2019-02-08 22:26:29 -0500

    Create base AppHeader component

---
+++ src/AppHeader/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+
+class AppHeader extends Component {
+  render() {
+    return <Typography>App Header</Typography>;
+  }
+}
+
+export default AppHeader;
