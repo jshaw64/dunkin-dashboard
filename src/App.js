a6576df70f74eb4cc9e67425635922db306e6051
jshaw <jshaw64@gmail.com>
2019-02-08 22:28:13 -0500

    Add AppHeader to App render

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './dd-logo.svg';
 import './App.css';
 
 import StatsSection from './StatsSection';
+import AppHeader from './AppHeader';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <AppHeader />
           <img src={logo} className="App-logo" alt="logo" />
           <StatsSection />
         </header>
