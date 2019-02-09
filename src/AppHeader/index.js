34fd14a6f02e51ecbb12f6c166b60dcfd7faeb3c
jshaw <jshaw64@gmail.com>
2019-02-08 22:30:12 -0500

    Display AppHeader text as h2

--- src/AppHeader/index.js
+++ src/AppHeader/index.js
@@ -3,7 +3,7 @@ import Typography from '@material-ui/core/Typography';
 
 class AppHeader extends Component {
   render() {
-    return <Typography>App Header</Typography>;
+    return <Typography variant="h2">App Header</Typography>;
   }
 }
 
